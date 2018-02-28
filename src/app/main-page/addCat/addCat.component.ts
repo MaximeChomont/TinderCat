b8427f34e4d374a99da05e14c024a3701295a412
FabienCampredon <campredon.fab@hotmail.fr>
2018-02-28 13:50:02 +0100

    meow4

--- src/app/main-page/addCat/addCat.component.ts
+++ src/app/main-page/addCat/addCat.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import {AuthenticationService} from "../../service/authentification.service";
+import { Cat } from '../../model/cat';
+import { CatService } from '../../service/model-service/cat.service';
 
 @Component({
   moduleId: module.id,
@@ -10,14 +12,20 @@ import {AuthenticationService} from "../../service/authentification.service";
 })
 
 export class AddCatComponent implements OnInit{
+cat: Cat;
 
   constructor(
     private route : ActivatedRoute,
     private router: Router,
+    private catService : CatService,
     private authenticationService: AuthenticationService
   ) {}
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.catService.addCat(this.cat).subscribe(cat => this.cat = cat);
   }
 
   logout() {
